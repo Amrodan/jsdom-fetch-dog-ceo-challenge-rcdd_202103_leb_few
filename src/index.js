cb292215f973c20ed1c6af057278c16c6a6b71f4
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 18:32:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-
-console.log('%c HI', 'color: firebrick')
-
- document.addEventListener("DOMContentLoaded", function a () {
-loadImages()
-
- });
-
-function loadImages() {
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-  fetch(imgUrl)
-    .then(res=> res.json())
-    .then(results => {
-      results.message.forEach(image => addImage(image))
-    });
-}
-function addImage(dogPicUrl) {
-  let container = document.querySelector('#dog-image-container');
-let newImageEl = document.createElement('img');
-newImageEl.src=dogPicUrl
-container.appendChild(newImageEl)
-}
-
-
-function loadbreed() {
-  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
-  document.getElementsById("dog-breeds")
-fetch(breedUrl)
-// .then(res => res.json())
-// .then ((json) => { console.log(json)
-//
+// 
+// console.log('%c HI', 'color: firebrick')
+// 
+//  document.addEventListener("DOMContentLoaded", function a () {
+// loadImages()
+// 
+//  });
+// 
+// function loadImages() {
+//   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+//   fetch(imgUrl)
+//     .then(res=> res.json())
+//     .then(results => {
+//       results.message.forEach(image => addImage(image))
+//     });
 // }
-
-}
+// function addImage(dogPicUrl) {
+//   let container = document.querySelector('#dog-image-container');
+// let newImageEl = document.createElement('img');
+// newImageEl.src=dogPicUrl
+// container.appendChild(newImageEl)
+// }
+// 
+// 
+// function loadbreed() {
+//   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+// 
+//   document.getElementsById("dog-breeds")
+// fetch(breedUrl)
+// // .then(res => res.json())
+// // .then ((json) => { console.log(json)
+// //
+// // }
+// 
+// }
