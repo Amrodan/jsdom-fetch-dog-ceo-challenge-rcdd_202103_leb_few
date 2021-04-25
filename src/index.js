f1dee106f4bce845d7ac4252d1c88bb243de46b5
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 18:59:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,42 +21,21 @@ newImageEl.src=dogPicUrl
 container.appendChild(newImageEl)
 }
 
-
+let breeds = [];
 function loadbreed() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
   document.getElementsById("dog-breeds")
 fetch(breedUrl)
-// .then(res => res.json())
-// .then ((json) => { console.log(json)
-//
-// }
 
+.then(res => res.json())
+.then (results => { 
+  breeds=Object.keys(results.message)
+
+});
 }
-// let breeds = [];
+
 //
-// document.addEventListener('DOMContentLoaded', function () {
-//   loadImages();
-//   loadBreedOptions();
-// });
-//
-// function loadImages() {
-//   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-//   fetch(imgUrl)
-//     .then(res=> res.json())
-//     .then(results => {
-//       results.message.forEach(image => addImage(image))
-//     });
-// }
-//
-// function addImage(dogPicUrl) {
-//   let container = document.querySelector('#dog-image-container');
-//   let newImageEl = document.createElement('img');
-//   newImageEl.src = dogPicUrl;
-//   container.appendChild(newImageEl);
-// }
-//
-// function loadBreedOptions() {
+ // function loadBreedOptions() {
 //   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 //   fetch(breedUrl)
 //     .then(res => res.json())
