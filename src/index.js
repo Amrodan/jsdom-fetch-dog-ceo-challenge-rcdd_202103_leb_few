85de1ba584af91f440539b97325dc92baf8a18aa
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 18:35:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -34,12 +34,12 @@ fetch(breedUrl)
 
 }
 // let breeds = [];
-// 
+//
 // document.addEventListener('DOMContentLoaded', function () {
 //   loadImages();
 //   loadBreedOptions();
 // });
-// 
+//
 // function loadImages() {
 //   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 //   fetch(imgUrl)
@@ -48,32 +48,32 @@ fetch(breedUrl)
 //       results.message.forEach(image => addImage(image))
 //     });
 // }
-// 
+//
 // function addImage(dogPicUrl) {
 //   let container = document.querySelector('#dog-image-container');
 //   let newImageEl = document.createElement('img');
 //   newImageEl.src = dogPicUrl;
 //   container.appendChild(newImageEl);
 // }
-// 
+//
 // function loadBreedOptions() {
 //   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 //   fetch(breedUrl)
 //     .then(res => res.json())
 //     .then(results => {
-// 
+//
 //       breeds = Object.keys(results.message);
 //       updateBreedList(breeds);
 //       addBreedSelectListener();
 //     });
 // }
-// 
+//
 // function updateBreedList(breeds) {
 //   let ul = document.querySelector('#dog-breeds');
 //   removeChildren(ul);
 //   breeds.forEach(breed => addBreed(breed));
 // }
-// 
+//
 // function removeChildren(element) {
 //   let child = element.lastElementChild;
 //   while (child) {
@@ -81,18 +81,18 @@ fetch(breedUrl)
 //     child = element.lastElementChild;
 //   }
 // }
-// 
+//
 // function selectBreedsStartingWith(letter) {
 //   updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
 // }
-// 
+//
 // function addBreedSelectListener() {
 //   let breedDropdown = document.querySelector('#breed-dropdown');
 //   breedDropdown.addEventListener('change', function (event) {
 //     selectBreedsStartingWith(event.target.value);
 //   });
 // }
-// 
+//
 // function addBreed(breed) {
 //   let ul = document.querySelector('#dog-breeds');
 //   let li = document.createElement('li');
@@ -101,8 +101,7 @@ fetch(breedUrl)
 //   ul.appendChild(li);
 //   li.addEventListener('click', updateColor);
 // }
-// 
+//
 // function updateColor(event) {
 //   event.target.style.color = 'palevioletred';
 // }
-
