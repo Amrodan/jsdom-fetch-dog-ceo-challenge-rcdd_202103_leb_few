fe45d902fcc9cd58d24a0accf944f36315829a21
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 18:32:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// 
+//
 // console.log('%c HI', 'color: firebrick')
-// 
+//
 //  document.addEventListener("DOMContentLoaded", function a () {
 // loadImages()
-// 
+//
 //  });
-// 
+//
 // function loadImages() {
 //   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 //   fetch(imgUrl)
@@ -20,16 +20,16 @@
 // newImageEl.src=dogPicUrl
 // container.appendChild(newImageEl)
 // }
-// 
-// 
+//
+//
 // function loadbreed() {
 //   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-// 
+//
 //   document.getElementsById("dog-breeds")
 // fetch(breedUrl)
 // // .then(res => res.json())
 // // .then ((json) => { console.log(json)
 // //
 // // }
-// 
+//
 // }
