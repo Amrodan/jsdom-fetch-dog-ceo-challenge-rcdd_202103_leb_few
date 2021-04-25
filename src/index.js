5617757158445cb7d51dc4d78c320132fc70ab02
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 17:51:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 //
-// console.log('%c HI', 'color: firebrick')
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-
+console.log('%c HI', 'color: firebrick')
+ 
  document.addEventListener("DOMContentLoaded", function a () {
 loadImages()
 
