d1f4b40dcabfebb063d35dfa33e1ba4e6a6cf6a5
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 20:49:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ function addBreed(breed) {
   ul.appendChild(li);
   // li.addEventListener('click', updateColor);
   li.addEventListener('click', function(){
-   event.target.style.color = 'palevioletred';
+   target.style.color = 'palevioletred';
  });
 }
 
