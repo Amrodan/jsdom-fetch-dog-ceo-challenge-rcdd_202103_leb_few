832c4b8436920caf104e0806fd53ae6c5a5d6e16
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 17:11:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
 const div = document.querySelector("#dog-image-container")
 // // document.addEventListener("DOMContentLoaded", function a () {
-    fetch(imgUrl)
-    .then((response) => response.json())
-       .then((json) => {
-         console.log(json);
-       a.message.forEach(function(messag)){
-
-
-          div.innerHTML+=
-            `<img src="${iimgUrl}">`
-            )
-          }  }
-;
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
