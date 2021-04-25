cfeb5c85b34de6ba6f82d31cd1c4adf0df3f9455
Amro dandashli <amrdandashli@gmail.com>
2021-04-25 20:29:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function addBreed(breed) {
   let ul = document.querySelector('#dog-breeds');
   let li = document.createElement('li');
   li.innerText = breed;
-  
+
   // li.style.cursor = 'pointer';
   ul.appendChild(li);
   // li.addEventListener('click', updateColor);
